bbda30d386283abc1ef6ed41a9f0129529351942
Julia Fernandes Ovidio <julia.ovidio@sou.unifeob.edu.br>
2025-11-17 20:44:25 -0300

    Delete electron_transportadora directory

--- electron_transportadora/preload.js
+++
@@ -1,6 +0,0 @@
-//conecao 
-const {contextBridge} = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  // Aqui você pode expor funções ou objetos para o contexto da janela
-});
--- electron_transportadora/renderer/public/js/login.js
+++
@@ -1,8 +0,0 @@
-function toggleSenha() {
-  const senhaInput = document.getElementById('senha');
-  if (senhaInput.type === 'password') {
-    senhaInput.type = 'text';
-  } else {
-    senhaInput.type = 'password';
-  }
-}
